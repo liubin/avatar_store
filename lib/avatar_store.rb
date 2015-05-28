d2e88c7778b4cc9d8be1f0670f3ded240d67aadc
bin liu <liubin0329@gmail.com>
2015-05-28 02:19:10 +0000

    update to 0.0.2, change avatar size and url pattern

--- lib/avatar_store.rb
+++ lib/avatar_store.rb
@@ -3,11 +3,16 @@ require 'digest/md5'
 
 module AvatarStore
 
-  FARM = "//farm.avatarstore.org"
+  FARM = "http://www.avatarstore.org"
+  VALID_SIZE = [64, 128, 256]
 
   def self.img_src(email, size = nil)
     key = "#{FARM}/#{Digest::MD5.hexdigest email.downcase}"
-    key = "#{key}_#{size}" if not size.nil?
+    if !size.nil? and VALID_SIZE.include?(size)
+      key = "#{key}/#{size}" if not size.nil?
+    else
+      key = "#{key}"
+    end
     key
   end
 
--- spec/avatar_store_spec.rb
+++ spec/avatar_store_spec.rb
@@ -7,6 +7,15 @@ describe AvatarStore do
 
   it 'should calculate md5 for an email' do
     expect(AvatarStore.img_src('liubin0329@gmail.com')).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71")
-    expect(AvatarStore.img_src('liubin0329@gmail.com', 32)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71_32")
+    expect(AvatarStore.img_src('liubin0329@gmail.com', 64)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71/64")
   end
+
+  it 'should have the extrat avatar size' do
+    expect(AvatarStore.img_src('liubin0329@gmail.com')).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71")
+    expect(AvatarStore.img_src('liubin0329@gmail.com', 64)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71/64")
+    expect(AvatarStore.img_src('liubin0329@gmail.com', 128)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71/128")
+    expect(AvatarStore.img_src('liubin0329@gmail.com', 256)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71/256")
+    expect(AvatarStore.img_src('liubin0329@gmail.com', 72)).to eq("#{AvatarStore::FARM}/2c07a4ee1a8447320fceb4a62989ed71")
+  end
+
 end
